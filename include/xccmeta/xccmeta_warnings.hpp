7956f5ca84a4b959865a807945e3c81c01ae125e
luppichristian <christian_luppi@outlook.com>
2026-01-29 16:32:02 +0100

    fixed warnings

--- include/xccmeta/xccmeta_warnings.hpp
+++ include/xccmeta/xccmeta_warnings.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "xccmeta_base.hpp"
+#include "xccmeta_source.hpp"
 
 namespace xccmeta {
 
@@ -8,15 +9,20 @@ namespace xccmeta {
   // It generates a string that contains definitions to trigger
   // warnings when compiled with specific compilers.
   // On MSVC it uses #pragma message, on GCC/Clang it uses #warning.
-  class compile_warnings {
+  class XCCMETA_API compile_warnings {
    public:
     compile_warnings() = default;
 
-    compile_warnings& push(const std::string& message);
+    compile_warnings& push(const std::string& message, source_location loc = {});
     std::string build() const;
 
    private:
-    std::vector<std::string> warnings;
+    struct entry {
+      std::string message;
+      source_location loc;
+    };
+
+    std::vector<entry> warnings;
   };
 
 }  // namespace xccmeta
--- src/xccmeta_warnings.cpp
+++ src/xccmeta_warnings.cpp
@@ -2,8 +2,8 @@
 
 namespace xccmeta {
 
-  compile_warnings& compile_warnings::push(const std::string& message) {
-    warnings.push_back(message);
+  compile_warnings& compile_warnings::push(const std::string& message, source_location loc) {
+    warnings.push_back({message, loc});
     return *this;
   }
 
@@ -12,14 +12,22 @@ namespace xccmeta {
       return "";
     }
 
-    std::string result;
+    std::string result = {};
     result += "#ifdef _MSC_VER\n";
     for (const auto& msg : warnings) {
-      result += "#pragma message(\"Warning: " + msg + "\")\n";
+      if (msg.loc.is_valid()) {
+        result += "#pragma message(\"Warning" + msg.loc.to_string() + ": " + msg.message + "\")\n";
+      } else {
+        result += "#pragma message(\"Warning: " + msg.message + "\")\n";
+      }
     }
     result += "#else\n";
     for (const auto& msg : warnings) {
-      result += "#warning \"" + msg + "\"\n";
+      if (msg.loc.is_valid()) {
+        result += "#warning \"" + msg.loc.to_string() + ": " + msg.message + "\"\n";
+      } else {
+        result += "#warning \"" + msg.message + "\"\n";
+      }
     }
     result += "#endif\n";
     return result;
